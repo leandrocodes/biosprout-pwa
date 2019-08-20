c4e5495b52fbbbb552290db05aae4f375e822bf1
pixel <pixel@pop-os.localdomain>
2019-08-20 02:23:04 -0400

    prettier

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,59 +1,67 @@
 <template>
-  <div class="login">
-    <h3>Sign In</h3>
-    <input type="text" v-model="email" placeholder="Email"><br>
-    <input type="password" v-model="password" placeholder="Password"><br>
-    <button @click="login">Connection</button>
-    <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
-  </div>
+    <div class="login">
+        <h3>Sign In</h3>
+        <input type="text" v-model="email" placeholder="Email" />
+        <br />
+        <input type="password" v-model="password" placeholder="Password" />
+        <br />
+        <button @click="login">Connection</button>
+        <p>
+            You don't have an account ? You can
+            <router-link to="/sign-up">create one</router-link>
+        </p>
+    </div>
 </template>
 
 <script>
-  import firebase from 'firebase';
+import firebase from "firebase"
 
-  export default {
-    name: 'login',
+export default {
+    name: "login",
     data() {
-      return {
-        email: '',
-        password: ''
-      }
+        return {
+            email: "",
+            password: ""
+        };
     },
     methods: {
-      login: function() {
-        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
-          (user) => {
-            this.$router.replace('home')
-          },
-          (err) => {
-            alert('Oops. ' + err.message)
-          }
-        );
-      }
+        login: function() {
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(this.email, this.password)
+                .then(
+                    () => {
+                        this.$router.replace("home")
+                    },
+                    err => {
+                        alert("Oops. " + err.message)
+                    }
+                )
+        }
     }
-  }
+}
 </script>
 
-<style scoped>  /* "scoped" attribute limit the CSS to this component only */
-  .login {
+<style scoped>
+.login {
     margin-top: 40px;
-  }
-  input {
+}
+input {
     margin: 10px 0;
     width: 20%;
     padding: 15px;
-  }
-  button {
+}
+button {
     margin-top: 20px;
     width: 10%;
     cursor: pointer;
-  }
-  p {
+}
+p {
     margin-top: 40px;
     font-size: 13px;
-  }
-  p a {
+}
+p a {
     text-decoration: underline;
     cursor: pointer;
-  }
+}
 </style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,56 +1,64 @@
 <template>
-  <div class="sign-up">
-    <p>Let's create a new account !</p>
-    <input type="text" v-model="email" placeholder="Email"><br>
-    <input type="password" v-model="password" placeholder="Password"><br>
-    <button @click="signUp">Sign Up</button>
-    <span>or go back to <router-link to="/login">login</router-link>.</span>
-  </div>
+    <div class="sign-up">
+        <p>Let's create a new account !</p>
+        <input type="text" v-model="email" placeholder="Email" />
+        <br />
+        <input type="password" v-model="password" placeholder="Password" />
+        <br />
+        <button @click="signUp">Sign Up</button>
+        <span>
+            or go back to
+            <router-link to="/login">login</router-link>.
+        </span>
+    </div>
 </template>
 
  <script>
-  import firebase from 'firebase';
+import firebase from "firebase"
 
-  export default {
-    name: 'signUp',
+export default {
+    name: "signUp",
     data() {
-      return {
-        email: '',
-        password: ''
-      }
+        return {
+            email: "",
+            password: ""
+        };
     },
     methods: {
-      signUp: function() {
-        firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
-          (user) => {
-            this.$router.replace('home')
-          },
-          (err) => {
-            alert('Oops. ' + err.message)
-          }
-        );
-      }
+        signUp: function() {
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.email, this.password)
+                .then(
+                    () => {
+                        this.$router.replace("home")
+                    },
+                    err => {
+                        alert("Oops. " + err.message)
+                    }
+                )
+        }
     }
-  }
+}
 </script>
 
  <style scoped>
-  .sign-up {
+.sign-up {
     margin-top: 40px;
-  }
-  input {
+}
+input {
     margin: 10px 0;
     width: 20%;
     padding: 15px;
-  }
-  button {
+}
+button {
     margin-top: 10px;
     width: 10%;
     cursor: pointer;
-  }
-  span {
+}
+span {
     display: block;
     margin-top: 20px;
     font-size: 11px;
-  }
+}
 </style>
