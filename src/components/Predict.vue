9eb7f28abf10f554770989a1d16460578a99d013
leandrocodes <leaxviana140@gmail.com>
2019-10-23 14:34:19 -0400

    fromPixel problem

--- src/components/Predict.vue
+++ src/components/Predict.vue
@@ -1,74 +1,73 @@
 <template>
-    <div class="hello">
-        <div class="file-upload-from">
-            <input type="file" @change="predictImage" accept="image/*" />
-            <div class="image-preview">
-                <img :src="imageData" class="preview">
-            </div>
-        </div>
+  <div class="hello">
+    <div class="file-upload-from">
+      <input type="file" @change="predictImage" accept="image/*" />
+      <div class="image-preview">
+        <img :src="imageData" class="preview" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import * as tf from "@tensorflow/tfjs"
 let model
-const loadModel = async () =>{
-    model = await tf.loadLayersModel('../json-model/model.json')
-    console.log(model)
+const loadModel = async () => {
+  model = await tf.loadLayersModel('../json-model/model.json')
+  console.log(model)
 }
-// let model await tf.loadLayersModel('../json-model/model.json')
 loadModel()
 
 export default {
-    data() {
-        return {
-            imageData: ""
-        }
-    },
-    methods: {
-        predictImage(event) {
-            //model = await tf.loadLayersModel('../json-model/model.json')
-            var input = event.target
-            if (input.files && input.files[0]) {
-                var reader = new FileReader()
-                reader.onload = e => {    
-                    this.imageData = e.target.result
-                }
-                reader.readAsDataURL(input.files[0])
-            }
-            // console.log(input.files)
-            const example = tf.fromPixels(this.imageData)
-            const prediction = model.predict(example)
-            console.log(prediction)
-        }
+  data () {
+    return {
+      imageData: ""
     }
+  },
+  methods: {
+    predictImage (event) {
+      //model = await tf.loadLayersModel('../json-model/model.json')
+      var input = event.target
+      if (input.files && input.files[0]) {
+        var reader = new FileReader()
+        reader.onload = e => {
+          this.imageData = e.target.result
+        }
+        reader.readAsDataURL(input.files[0])
+      }
+      // console.log(input.files)
+      const example = tf.fromPixels(this.imageData)
+      const prediction = model.predict(example)
+      // console.log(prediction)
+    }
+  }
 }
 </script>
 
 <style scoped>
 h3 {
-    margin: 40px 0 0;
+  margin: 40px 0 0;
 }
 ul {
-    list-style-type: none;
-    padding: 0;
+  list-style-type: none;
+  padding: 0;
 }
 li {
-    display: inline-block;
-    margin: 0 10px;
+  display: inline-block;
+  margin: 0 10px;
 }
 a {
-    color: #42b983;
+  color: #42b983;
 }
-.file-upload-form, .image-preview {
-    font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
-    padding: 20px;
+.file-upload-form,
+.image-preview {
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  padding: 20px;
 }
 img.preview {
-    width: 200px;
-    background-color: white;
-    border: 1px solid #DDD;
-    padding: 5px;
+  width: 200px;
+  background-color: white;
+  border: 1px solid #ddd;
+  padding: 5px;
 }
-
 </style>
